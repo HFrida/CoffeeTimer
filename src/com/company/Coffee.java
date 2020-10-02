1f06f29764a8010a5905345ce7317f3c60618d72
Frida Hammarberg <frida.hammarberg@mensa.se>
2020-10-02 11:00:39 +0200

    ArrayList with coffee and TimerApplication-class

--- src/com/company/Coffee.java
+++ src/com/company/Coffee.java
@@ -4,4 +4,18 @@ public class Coffee {
 
     private String producer;
     private String product;
+    private String information;
+
+    public Coffee(String producer, String product, String information) {
+        this.producer = producer;
+        this.product = product;
+        this.information = information;
+    }
+
+    @Override
+    public String toString() {
+        return "\nProducer: " + producer +
+                "\nProduct: " + product +
+                "\nInformation: " + information + "\n";
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        TimerApplication timerApplication = new TimerApplication();
+
+        timerApplication.showCoffee();
+
     }
 }
